8efb90f1cbb83c899122fb9fb3661b7f888a2569
a8anassis <a8anassis@gmail.com>
2026-07-10 18:32:55 +0300

    style(ch4): Format array spread syntax for improved readability

--- ch4-arrays/spreading.js
+++ ch4-arrays/spreading.js
@@ -1,8 +1,8 @@
 const initial = [1, 2, 3]
-const copy = [...initial]
+const copy = [ ...initial ]
 
-const extended = [0, ...initial, 4]
-const chars = [..."Hello"]
+const extended = [ 0, ...initial, 4 ]
+const chars = [ ..."Hello" ]
 
 console.log(copy)
 console.log(extended)
