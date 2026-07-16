2e8a1a9e67d8cf646bb6c430e9921b929909c811
a8anassis <a8anassis@gmail.com>
2026-07-16 18:56:35 +0300

    feat(ch3): Add palindrome functions to check for palindromes in strings

--- ch3-strings/review.js
+++ ch3-strings/review.js
@@ -19,3 +19,25 @@ function reverse2(str) {
 
 let str = "Coding"
 console.log(reverse(str))
+
+
+// Άσκηση 2
+// Μία συνάρτηση που να ελέγχει αν ένα string
+// είναι παλίνδρομο, αγνοώντας πεζά/κεφαλαία και κενά.
+// Επιστρέφει boolean
+
+function palindrome(str) {
+    const cleaned = str.trim().toUpperCase().split(/\s+/).join('')
+
+    for (let i = 0, j = cleaned.length - 1; i < j; i++, j--) {
+        if (cleaned[i] !== cleaned[j]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function palindrome2(str) {
+    const reversed = str.trim().toUpperCase().split(/\s+/).reverse().join('')
+    return reversed === str.toUpperCase()
+}
